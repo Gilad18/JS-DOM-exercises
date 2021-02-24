be2b1884f2cbb3fa0de9ca0609521ebf9c96e365
Gilad <gilad18587@gmail.com>
2021-02-24 15:43:40 +0200

    update 23.1

--- 23.1/app.js
+++ 23.1/app.js
@@ -1,28 +1,39 @@
 const hour = document.querySelector('.hh');
 const minute = document.querySelector('.mm');
 const second = document.querySelector('.ss');
+const pointhour = document.querySelector('.hourMehoga');
+const pointMin = document.querySelector('.minMehoga');
+const pointSec = document.querySelector('.secMehoga');
 
 let theSeconds = 0;
 let theMinutes = 0;
 let theHours = 0;
+let theSecDegree = 0;
+let theMinDegree = 0;
+let theHourDegree = 0;
 minute.innerHTML = theMinutes;
 hour.innerHTML = theHours;  
 
 setInterval(() => {
     if (theSeconds === 59) {
         theSeconds = -1;
+        theSecDegree = -6 ;
     }
     theSeconds = theSeconds + 1;
+    theSecDegree = theSecDegree + 6;
     second.innerHTML = theSeconds;
+    pointSec.style.transform = `rotate(${theSecDegree}deg)`
     
 } , 1000);
 
 setInterval(() => {
     if (theMinutes === 59) {
-        theMinutes = - 1
+        theMinutes = - 1;
     }
     theMinutes = theMinutes + 1;
     minute.innerHTML = theMinutes;
+    theMinDegree = theMinDegree + 6;
+    pointMin.style.transform = `rotate(${theMinDegree}deg)`
 } , 60000);
 
 setInterval(() => {
@@ -31,5 +42,7 @@ setInterval(() => {
     }
     theHours = theHours + 1;
     hour.innerHTML = theHours;
+    theHourDegree = theHourDegree + 15;
+    pointhour.style.transform = `rotate(${theHourDegree}deg)`
 } , 3600000);
 
---
+++ 23.2/app.js
@@ -0,0 +1,12 @@
+const minute = document.querySelector('.min');
+const second = document.querySelector('.sec');
+const miliSec = document.querySelector('.mils');
+
+
+let theMinute = 0;
+let theSecond = 0;
+let theMilSecond = 0;
+
+minute.innerHTML = theMinute;
+second.innerHTML = theSecond;
+miliSec.innerHTML = theMilSecond;
